5831b2c1fde627b03b708bec8a3a826c90e779e5
Nowik434 <nowik434@gmail.com>
2021-08-30 20:20:59 +0200

    add gallery to netify 0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,11 +87,15 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-theme-ui",
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      }
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
         trackingId: settings.ga,
       },
     },
