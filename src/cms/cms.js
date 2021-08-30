1892487a11bf9480359d3601783cb0f82345dc85
Nowik434 <nowik434@gmail.com>
2021-08-30 17:47:08 +0200

    add gallery to netify 0.1

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,9 @@
-import CMS from 'netlify-cms-app'
+import CMS from 'netlify-cms-app';
+
+// You only need to import the media library that you'll use. We register both
+// here for example purposes.
 import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
